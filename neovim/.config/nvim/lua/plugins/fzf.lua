584d2cdd48432ff2b12e168bbe8be3d594c07e86
egoreast <i@egoreast.ru>
2025-07-31 13:12:38 +0300

    update fzf actions and update packages in nvim

---
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,27 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = function(_, opts)
+    local fzf = require("fzf-lua")
+    local actions = fzf.actions
+    local config = fzf.config
+
+    opts.files = opts.files or {}
+    -- opts.files.cmd = "fd --type f --hidden --no-ignore --follow --exclude .git"
+    opts.files.actions = {
+      ["ctrl-i"] = actions.toggle_ignore,
+      ["ctrl-h"] = actions.toggle_hidden,
+    }
+
+    opts.grep = opts.grep or {}
+    -- opts.grep.rg_opts = "--hidden --no-ignore --column --line-number --no-heading --color=always --smart-case --trim"
+    opts.grep.actions = {
+      ["ctrl-i"] = actions.toggle_ignore,
+      ["ctrl-h"] = actions.toggle_hidden,
+    }
+
+    config.set_action_helpstr(actions.toggle_ignore, "toggle-ignore")
+    config.set_action_helpstr(actions.toggle_hidden, "toggle-hidden")
+
+    return opts
+  end,
+}
