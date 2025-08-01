80b0d4d220553d426c26975f857bf60f18890ac2
egoreast <i@egoreast.ru>
2025-08-01 10:29:03 +0300

    add fg package via ya pkg

--- yazi/.config/yazi/plugins/fg.yazi/main.lua
+++ yazi/.config/yazi/plugins/fg.yazi/main.lua
@@ -176,7 +176,7 @@ function M:entry(job)
 	end
 
 	local child, err =
-		Command(shell_value):args({"-c", cmd_args}):cwd(cwd):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
+		Command(shell_value):arg({"-c", cmd_args}):cwd(cwd):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
 
 	if not child then
 		return fail("Spawn `rfzf` failed with error code %s. Do you have it installed?", err)
@@ -228,7 +228,7 @@ function M:entry(job)
 	if (default_action == "nvim" or get_option() == "nvim" ) and args[1] ~= "fzf" then
 		os.execute("nvim +"..line_number.." -n "..file_url)
 	elseif (default_action == "helix" or get_option() == "helix" ) and args[1] ~= "fzf" then
-		os.execute("hx +"..line_number.." "..file_url)
+		os.execute("helix +"..line_number.." "..file_url)
 	elseif (default_action == "jump" or get_option() == "jump" or args[1] == "fzf") and file_url ~= ""  then
 		ya.mgr_emit(file_url:match("[/\\]$") and "cd" or "reveal", { file_url })
 	else
