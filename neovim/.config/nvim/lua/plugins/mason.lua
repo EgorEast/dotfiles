cad5b04ffd40f52c5a24dc1a2c9c850d888d0004
egoreast <i@egoreast.ru>
2025-08-09 21:51:34 +0300

    update nvim mason and packages

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -3,7 +3,6 @@ return {
   opts = {
     ensure_installed = {
       "emmet-language-server",
-      "chrome-debug-adapter",
       "firefox-debug-adapter",
       "js-debug-adapter",
       "lua-language-server",
