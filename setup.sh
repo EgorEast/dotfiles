f37610495c9612aaabd76646bff1b2aa531dc95f
egoreast <i@egoreast.ru>
2025-08-21 00:10:44 +0300

    update setup script

--- setup.sh
+++ setup.sh
@@ -8,38 +8,188 @@ echo ">>> Creating symlinks with stow..."
 cd ~/.local/src/dotfiles
 
 stow --adopt --restow \
-  anydesk autostart bash calcurse cmus curl delta dunst fastfetch fish flameshot \
-  gemini git glow greenclip gtk i3 icons inputrc kitty lazygit mineapp-list mpv \
-  nano nekoray neovim nwg-look obs onlyoffice pavucontrol picom pipewire rofi \
-  rudesktop spectacle ssh thunar user-dirs vim wget xfce-4 xinit xorg xsettingsd \
-  ya-disk ya-music yazi yt-dlp ytsurf xarchiver galculator lazydocker redshift \
-  ncdu bluetuith lsd qalculate bat tg mailcap feh sc-im ovpn gobang -t ~
+  anydesk \
+  autostart \
+  bash \
+  bat \
+  bluetuith \
+  calcurse \
+  cmus \
+  curl \
+  delta \
+  dunst \
+  fastfetch \
+  feh \
+  fish \
+  flameshot \
+  galculator \
+  gemini \
+  git \
+  glow \
+  gobang \
+  greenclip \
+  gtk \
+  i3 \
+  icons \
+  inputrc \
+  kitty \
+  lazydocker \
+  lazygit \
+  lsd \
+  mailcap \
+  mineapp-list \
+  mpv \
+  nano \
+  ncdu \
+  nekoray \
+  neovim \
+  nwg-look \
+  obs \
+  onlyoffice \
+  ovpn \
+  pavucontrol \
+  picom \
+  pipewire \
+  qalculate \
+  redshift \
+  rofi \
+  rudesktop \
+  sc-im \
+  spectacle \
+  ssh \
+  tg \
+  thunar \
+  user-dirs \
+  vim \
+  wget \
+  xarchiver \
+  xfce-4 \
+  xinit \
+  xorg \
+  xsettingsd \
+  ya-disk \
+  ya-music \
+  yazi \
+  yt-dlp \
+  ytsurf \
+  -t ~
+
+echo ">>> Installing base packages..."
+sudo pacman -S --noconfirm \
+  bandwhich \
+  bat \
+  blueberry \
+  brightnessctl \
+  calcurse \
+  cloc \
+  cmus \
+  ddcutil \
+  docker \
+  docker-compose \
+  fastfetch \
+  fd \
+  feh \
+  firefox \
+  fish \
+  fisher \
+  flameshot \
+  galculator \
+  git-delta \
+  glow \
+  gnome-keyring \
+  gparted \
+  gping \
+  gsimplecal \
+  hledger \
+  hledger-ui \
+  jq \
+  kitty \
+  lazygit \
+  libqalculate \
+  libsecret \
+  lsd \
+  ncdu \
+  networkmanager-openvpn \
+  nodejs-lts-jod \
+  npm \
+  nvim \
+  obs-studio \
+  obsidian \
+  onefetch \
+  oryx \
+  ouch \
+  picom \
+  playerctl \
+  python-pipx \
+  qbittorrent \
+  redshift \
+  ripgrep \
+  rustup \
+  seahorse \
+  shotcut \
+  spectacle \
+  tabiew \
+  telegram-desktop \
+  tor \
+  torsocks \
+  trash-cli \
+  tree \
+  trippy \
+  ttf-jetbrains-mono-nerd \
+  urlview \
+  vim \
+  vulkan-icd-loader \
+  vulkan-radeon \
+  vulkan-tools \
+  wine \
+  xclip \
+  xsel \
+  yt-dlp \
+  zoxide
+
+echo ">>> Installing AUR packages (via yay)..."
+yay -S --noconfirm \
+  anydesk-bin \
+  bitchat-tui \
+  blobdrop-git \
+  bluetuith-bin \
+  downloader-cli \
+  dysk \
+  fish-done \
+  gobang-bin \
+  hellwal \
+  lazydocker \
+  mmtui-bin \
+  nekoray-bin \
+  obfs4proxy \
+  onlyoffice-bin \
+  pandoc-bin \
+  pantum-driver \
+  portproton \
+  puffin \
+  rofi-bluetooth-git \
+  rofi-games \
+  rofi-greenclip \
+  rudesktop \
+  sc-im \
+  speedread-git \
+  sysz \
+  telegram-tg \
+  torbrowser-launcher \
+  ventoy-bin \
+  visual-studio-code-bin \
+  whatsapp-linux-desktop \
+  xautolock \
+  xkblayout-state-git \
+  yandex-browser \
+  yandex-disk \
+  yandex-music \
+  yazi-git \
+  ytsurf
 
 echo ">>> Installing neovim..."
 nvim --headless "+Lazy! sync" +qa
 nvim --headless "+Lazy! load mason.nvim" "+lua require('mason.api.command').MasonUpdate()" +qa
-
-echo ">>> Installing base packages..."
-sudo pacman -S --noconfirm \
-  vim nodejs-lts-jod npm kitty ttf-jetbrains-mono-nerd \
-  fish fisher nvim lazygit git-delta trash-cli zoxide ouch glow onefetch \
-  ripgrep xclip xsel cmus lsd playerctl jq gparted qbittorrent spectacle \
-  obs-studio networkmanager-openvpn yt-dlp shotcut redshift blueberry \
-  gsimplecal calcurse telegram-desktop libsecret gnome-keyring seahorse \
-  ddcutil firefox brightnessctl flameshot galculator tree fastfetch picom \
-  obsidian vulkan-radeon vulkan-tools vulkan-icd-loader cloc bat tabiew \
-  tor torsocks ncdu fd rustup wine python-pipx libqalculate bandwhich urlview \
-  feh docker docker-compose gping hledger hledger-ui oryx trippy
-
-echo ">>> Installing AUR packages (via yay)..."
-yay -S --noconfirm \
-  yazi-git fish-done yandex-browser onlyoffice-bin portproton ventoy-bin \
-  pantum-driver yandex-disk visual-studio-code-bin xkblayout-state-git \
-  rofi-greenclip rudesktop anydesk-bin xautolock nekoray-bin \
-  whatsapp-linux-desktop ytsurf lazydocker dysk rofi-games blobdrop-git \
-  bitchat-tui downloader-cli hellwal speedread-git torbrowser-launcher \
-  obfs4proxy yandex-music rofi-bluetooth-git bluetuith-bin mmtui-bin \
-  telegram-tg sc-im pandoc-bin gobang-bin puffin sysz
 
 echo ">>> Installing global npm packages..."
 sudo npm i -g npm-check-updates @bramus/caniuse-cli @google/gemini-cli pnq
