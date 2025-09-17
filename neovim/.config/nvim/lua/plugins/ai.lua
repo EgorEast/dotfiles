d12ff7ae1abb04d206f001fb5895033022e30ad5
egoreast <i@egoreast.ru>
2025-09-17 22:50:29 +0300

    add ai plugins to ndim and update crush config, but its not work with ollaba now

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -12,6 +12,72 @@ return {
       debug = false, -- enable debug logging
     },
   },
+  {
+    "nomnivore/ollama.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+
+    -- All the user commands added by the plugin
+    cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+    keys = {
+      -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+      {
+        "<leader>oo",
+        ":<c-u>lua require('ollama').prompt()<cr>",
+        desc = "ollama prompt",
+        mode = { "n", "v" },
+      },
+
+      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+      {
+        "<leader>oG",
+        ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+        desc = "ollama Generate Code",
+        mode = { "n", "v" },
+      },
+    },
+
+    ---@type Ollama.Config
+    opts = {
+      -- your configuration overrides
+    },
+  },
+  -- https://github.com/magicalne/nvim.ai?tab=readme-ov-file#config
+  -- {
+  --   "magicalne/nvim.ai",
+  --   version = "*",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   opts = {
+  --     provider = "ollama", -- You can configure your provider, model or keymaps here.
+  --     gemini = {
+  --       endpoint = "https://generativelanguage.googleapis.com",
+  --       model = "gemini-1.5-flash-latest", -- or gemini-1.5-pro-latest
+  --       temperature = 0,
+  --       max_tokens = 4096,
+  --       ["local"] = false,
+  --     },
+  --     ollama = {
+  --       ["local"] = true,
+  --       model = "deepseek-coder-v2", -- You can start with smaller one like `gemma2` or `llama3.1`
+  --       endpoint = "http://192.168.2.47:11434", -- In case you access ollama from another machine
+  --     },
+  --     keymaps = {
+  --       toggle = "<leader>ccc", -- Toggle chat dialog
+  --       send = "<CR>", -- Send message in normal mode
+  --       close = "q", -- Close chat dialog
+  --       clear = "<C-l>", -- Clear chat history
+  --       stop_generate = "<C-c>", -- Stop generating
+  --       previous_chat = "<leader>[", -- Open previous chat from history
+  --       next_chat = "<leader>]", -- Open next chat from history
+  --       inline_assist = "<leader>i", -- Run InlineAssist command with prompt
+  --     },
+  --   },
+  -- },
   -- https://github.com/GeorgesAlkhouri/nvim-aider
   -- {
   --   "GeorgesAlkhouri/nvim-aider",
