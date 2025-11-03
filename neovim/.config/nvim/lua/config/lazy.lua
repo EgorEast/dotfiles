b4eaf819c521762fa56be8731f7b3e1b02753be1
egoreast <i@egoreast.ru>
2025-11-03 15:01:55 +0300

    remove test plugins from nvim and update packages

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -47,7 +47,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.test.core" },
+    -- { import = "lazyvim.plugins.extras.test.core" },
     -- { import = "lazyvim.plugins.extras.ui.edgy" },
     -- { import = "lazyvim.plugins.extras.ui.smear-cursor" },
     -- { import = "lazyvim.plugins.extras.util.chezmoi" },
