c2569332ec00de42fcdd7ea0ac764e8ac878d45c
egoreast <i@egoreast.ru>
2025-11-24 14:47:30 +0300

    add ssetup minimal setup termux script

---
+++ scripts/setup_minimal_termux.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+pkg install stow
+
+echo ">>> Creating symlinks with stow..."
+cd ~/.local/src/dotfiles
+
+stow --adopt --restow \
+  bash \
+  bat \
+  btop \
+  curl \
+  delta \
+  fastfetch \
+  fish \
+  glow \
+  jqp \
+  lazygit \
+  mpv \
+  nano \
+  ncdu \
+  yazi \
+  yt-dlp \
+  -t ~
+
+echo ">>> Installing base packages..."
+
+pkg install \
+  bat \
+  btop \
+  curl \
+  fastfetch \
+  fd \
+  fish \
+  fisher \
+  fx \
+  git \
+  git-delta \
+  glow \
+  lazygit \
+  lsd \
+  ncdu \
+  nodejs-lts-jod \
+  npm \
+  nvim \
+  onefetch \
+  ouch \
+  ripgrep \
+  trash-cli \
+  tree \
+  ttf-jetbrains-mono-nerd \
+  xclip \
+  xsel \
+  yazi \
+  zoxide
+
+echo ">>> Installing global npm packages..."
+sudo npm i -g @bramus/caniuse-cli
+
+echo ">>> All done!"
