95da6bbcd42e05d924ee39251d652d2e270773fe
egoreast <i@egoreast.ru>
2025-11-30 22:23:30 +0300

    update setup scipt

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "Select setup type:"
-echo "1) Full setup"
-echo "2) Minimal setup (default)"
+echo "1) Minimal setup (default)"
+echo "2) Full setup"
 read -r -p "Enter choice (1 or 2, press Enter for minimal): " choice
 
-# If no input, default to 2
-choice=${choice:-2}
+# If no input, default to 1
+choice=${choice:-1}
 
 case $choice in
-1)
-  echo "Running full setup..."
-  # === Commands for full setup ===
-  . ./scripts/setup_full.sh
-  echo "Full setup completed."
-  ;;
-2 | "")
+1 | "")
   echo "Running minimal setup..."
   # === Commands for minimal setup ===
   . ./scripts/setup_minimal.sh
   echo "Minimal setup completed."
+  ;;
+2)
+  echo "Running full setup..."
+  # === Commands for full setup ===
+  . ./scripts/setup_full.sh
+  echo "Full setup completed."
   ;;
 *)
   echo "Invalid choice. Exiting."
