02f23dfaf86ba10ba47036ba9955380c0f9f9e20
egoreast <i@egoreast.ru>
2025-12-22 12:17:26 +0300

    update screenlayout in i3

--- i3/.config/i3/screenlayout/with-second-screen-laptop.sh
+++ i3/.config/i3/screenlayout/with-second-screen-laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --mode 1920x1080 --pos 0x472 --rotate normal --output HDMI-A-0 --primary --mode 2560x1440 --pos 1920x0 --rotate left --output DisplayPort-0 --off --output DisplayPort-1 --off
+xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 2560x1440 --pos 1920x0 --rotate left --output DisplayPort-0 --off --output DisplayPort-1 --off
