8a831186be4175c0e8e7f84a563f6c0c9d2a1ff8
egoreast <i@egoreast.ru>
2026-01-20 14:53:50 +0300

    add noconfirm and needed flags for installing stow

--- scripts/setup_main_start.sh
+++ scripts/setup_main_start.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo ">>> Updating system..."
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S stow
+sudo pacman -S --noconfirm --needed stow
 
 echo ">>> Creating symlinks with stow..."
 cd ~/.local/src/dotfiles
