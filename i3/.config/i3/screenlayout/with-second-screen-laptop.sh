5e935d2c187bf1f1a1b2b79aadf74726c85cdbed
egoreast <i@egoreast.ru>
2026-01-26 17:40:38 +0300

    remove primary parks from i3 sceenlayout script

--- i3/.config/i3/screenlayout/with-second-screen-laptop.sh
+++ i3/.config/i3/screenlayout/with-second-screen-laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 2560x1440 --pos 1920x0 --rotate left --output DisplayPort-0 --off --output DisplayPort-1 --off
+xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS-1 --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 2560x1440 --pos 1920x0 --rotate left --output DisplayPort-0 --off --output DisplayPort-1 --off
