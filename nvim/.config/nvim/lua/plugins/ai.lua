61521a0750d75cb458ad34adf6a4466b6bd2e5fe
egoreast <i@egoreast.ru>
2026-02-10 15:56:33 +0300

    add opencode and update packages in nvim

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -45,4 +45,58 @@ return {
       })
     end,
   },
+  {
+    "nickjvandyke/opencode.nvim",
+    dependencies = {
+      -- Recommended for `ask()` and `select()`.
+      -- Required for `snacks` provider.
+      ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
+        -- provider = {
+        --   enabled = "kitty",
+        --   kitty = {
+        --     -- ...
+        --   },
+        -- },
+      }
+
+      -- Required for `opts.events.reload`.
+      vim.o.autoread = true
+
+      local map = vim.keymap.set
+
+      map({ "n", "x" }, "<leader>a", "", { desc = "AI" })
+      map({ "n", "x" }, "<leader>ao", "", { desc = "Opencode" })
+
+      -- Recommended/example keymaps.
+      map({ "n", "x" }, "<leader>aoa", function()
+        require("opencode").ask("@this: ", { submit = true })
+      end, { desc = "Ask opencode…" })
+      map({ "n", "x" }, "<leader>aox", function()
+        require("opencode").select()
+      end, { desc = "Execute opencode action…" })
+      map({ "n", "t" }, "<leader>ao.", function()
+        require("opencode").toggle()
+      end, { desc = "Toggle opencode" })
+
+      map({ "n", "x" }, "go", function()
+        return require("opencode").operator("@this ")
+      end, { desc = "Add range to opencode", expr = true })
+      map("n", "goo", function()
+        return require("opencode").operator("@this ") .. "_"
+      end, { desc = "Add line to opencode", expr = true })
+
+      map("n", "<S-C-u>", function()
+        require("opencode").command("session.half.page.up")
+      end, { desc = "Scroll opencode up" })
+      map("n", "<S-C-d>", function()
+        require("opencode").command("session.half.page.down")
+      end, { desc = "Scroll opencode down" })
+    end,
+  },
 }
