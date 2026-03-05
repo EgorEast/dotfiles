a3e07e90808c365c358ecd0b352001decb0e9d6c
egoreast <i@egoreast.ru>
2026-03-05 12:59:05 +0300

    update ollama installation models

--- scripts/install_llms.sh
+++ scripts/install_llms.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-ollama pull deepseek-coder-v2 &
-ollama pull gpt-oss &
+# ollama pull deepseek-coder-v2 &
+# ollama pull gpt-oss &
+ollama pull gemma3:4b &
+ollama pull embeddinggemma &
+# ollama pull qwen3-embedding &
 ollama pull qwen3-coder
