fe7bd2d1f862e7a06f0bc2a1080dbcdd28bf656a
egoreast <i@egoreast.ru>
2026-04-02 16:02:45 +0300

    add script for granting gps permission on android

---
+++ scripts/grant_gps_permissions_on_android.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+adb shell pm grant com.google.android.gms android.permission.ACCESS_COARSE_LOCATION
+adb shell pm grant com.google.android.gms android.permission.ACCESS_FINE_LOCATION
