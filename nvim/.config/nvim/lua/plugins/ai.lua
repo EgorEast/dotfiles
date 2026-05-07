efa4041d759cbe985033776b4a897505aab936bb
egoreast <i@egoreast.ru>
2026-05-07 10:32:53 +0300

    update ai.lua in nvim plugins

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,27 +1,28 @@
 return {
   {
     "nomnivore/ollama.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+    dependencies = { "nvim-lua/plenary.nvim" },
 
     -- All the user commands added by the plugin
     cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
 
     keys = {
-      -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
       {
-        "<leader>oo",
-        ":<c-u>lua require('ollama').prompt()<cr>",
-        desc = "ollama prompt",
+        "<leader>al",
+        "",
+        desc = "Ollama",
         mode = { "n", "v" },
       },
-
-      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
       {
-        "<leader>oG",
+        "<leader>alg",
         ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-        desc = "ollama Generate Code",
+        desc = "Ollama Generate Code",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>alp",
+        ":<c-u>lua require('ollama').prompt()<cr>",
+        desc = "Ollama prompt",
         mode = { "n", "v" },
       },
     },
@@ -33,16 +34,9 @@ return {
   },
   -- {
   --   "Exafunction/windsurf.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "hrsh7th/nvim-cmp",
-  --   },
+  --   dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp" },
   --   config = function()
-  --     require("codeium").setup({
-  --       virtual_text = {
-  --         enabled = true,
-  --       },
-  --     })
+  --     require("codeium").setup({ virtual_text = { enabled = true } })
   --   end,
   -- },
   {
@@ -63,11 +57,7 @@ return {
               end,
             },
             win = {
-              input = {
-                keys = {
-                  ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
-                },
-              },
+              input = { keys = { ["<a-a>"] = { "opencode_send", mode = { "n", "i" } } } },
             },
           },
           terminal = {}, -- Enables the `snacks` provider
@@ -129,81 +119,4 @@ return {
       })
     end,
   },
-
-  -- {
-  --   "yetone/avante.nvim",
-  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  --   -- ⚠️ must add this setting! ! !
-  --   build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-  --     or "make",
-  --   event = "VeryLazy",
-  --   version = false, -- Never set this value to "*"! Never!
-  --   ---@module 'avante'
-  --   ---@type avante.Config
-  --   opts = {
-  --     -- add any opts here
-  --     -- this file can contain specific instructions for your project
-  --     instructions_file = "avante.md",
-  --     -- for example
-  --     --     provider = "claude",
-  --     --     providers = {
-  --     --       claude = {
-  --     --         endpoint = "https://api.anthropic.com",
-  --     --         model = "claude-sonnet-4-20250514",
-  --     --         timeout = 30000, -- Timeout in milliseconds
-  --     --         extra_request_body = {
-  --     --           temperature = 0.75,
-  --     --           max_tokens = 20480,
-  --     --         },
-  --     --       },
-  --     --       moonshot = {
-  --     --         endpoint = "https://api.moonshot.ai/v1",
-  --     --         model = "kimi-k2-0711-preview",
-  --     --         timeout = 30000, -- Timeout in milliseconds
-  --     --         extra_request_body = {
-  --     --           temperature = 0.75,
-  --     --           max_tokens = 32768,
-  --     --         },
-  --     --       },
-  --     --     },
-  --   },
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     --- The below dependencies are optional,
-  --     -- "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-  --     -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-  --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-  --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
-  --     "stevearc/dressing.nvim", -- for input provider dressing
-  --     "folke/snacks.nvim", -- for input provider snacks
-  --     -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-  --     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --     {
-  --       -- support for image pasting
-  --       "HakonHarnes/img-clip.nvim",
-  --       event = "VeryLazy",
-  --       opts = {
-  --         -- recommended settings
-  --         default = {
-  --           embed_image_as_base64 = false,
-  --           prompt_for_file_name = false,
-  --           drag_and_drop = {
-  --             insert_mode = true,
-  --           },
-  --           -- required for Windows users
-  --           use_absolute_path = true,
-  --         },
-  --       },
-  --     },
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = {
-  --         file_types = { "markdown", "Avante" },
-  --       },
-  --       ft = { "markdown", "Avante" },
-  --     },
-  --   },
-  -- },
 }
