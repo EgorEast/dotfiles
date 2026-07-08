e9b2ad567104da50f8bc3eda6163e33da893750f
egoreast <i@egoreast.ru>
2026-07-08 10:31:17 +0300

    add dart-debug-adapter in mason nvim

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -2,17 +2,18 @@ return {
   "mason-org/mason.nvim",
   opts = {
     ensure_installed = {
+      "css-lsp",
+      "css-variables-language-server",
+      "dart-debug-adapter",
       "emmet-language-server",
       "firefox-debug-adapter",
+      "html-lsp",
       "js-debug-adapter",
+      "json-lsp",
       "lua-language-server",
       "shellcheck",
       "shfmt",
       "stylua",
-      "css-lsp",
-      "html-lsp",
-      "json-lsp",
-      "css-variables-language-server",
     },
   },
 }
